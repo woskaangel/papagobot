4168e1e55c9c5a061a0057aafd8375731725ec9e
woskaangel <52520428+woskaangel@users.noreply.github.com>
2020-10-05 18:14:39 +0900

    Update files
    - delete database file
    - add yarn nodemon module
    - edit help and index
    - update translate

--- commands/translate.js
+++ commands/translate.js
@@ -1,28 +1,96 @@
 const papago=require('../api/papago');
 
-exports.translate=async(msg,embed,discord)=>{
-    // 한국 중국 영어 일본어 스페인 프랑스 이탈리아 러시아 독일어 태국 베트남 인도네시아
+exports.translate=async(msg,embed)=>{
+    if(msg.content==="파파고"){
+        await embed.setTitle(`번역 될 언어를 선택하세요.`);
+    } else if(msg.content==="papago"){
+        await embed.setTitle(`Please select a language to be translated.`);
+    } else{
+        return;
+    }
+    let sendMsg=await msg.channel.send(embed);
+
     const iconList=['🇰🇷','🇨🇳','🇬🇧','🇯🇵','🇪🇸','🇫🇷','🇮🇹','🇷🇺','🇩🇪','🇹🇭','🇻🇳','🇮🇩'];
-
-    const filter=(reaction,user)=>{return iconList.includes(reaction.emoji.name)&&user.id===msg.author.id};
-
-    if(msg.content==="파파고") embed.setTitle(`번역 될 언어를 선택하세요.`);
-    else if(msg.content==="papago") embed.setTitle(`Please select a language to be translated.`);
-
-    msg.channel.send(embed).then(reply=>{
-        for(const i in iconList) reply.react(iconList[i]);
-        reply.awaitReactions(filter,{MAX: 1,time: 12000}).then(data=>{
-            const reaction=data.first();
-            console.log(reaction);
-        }).catch(e=>{throw e;});
-    }).catch(e=>{throw e;});
-    /*
-    const text="안녕하세요";
-    const LanguageCode=await papago.DetectLanguage(text);
-    const TransText=await papago.Translation(LanguageCode.langCode,"en",text);
-    embed.setTitle(`${TransText.message.result.srcLangType} → ${TransText.message.result.tarLangType}`)
-    .addField(`${TransText.message.result.srcLangType} to translate`,text)
-    .addField(`Translated ${TransText.message.result.tarLangType}`,TransText.message.result.translatedText);
-    msg.channel.send(embed);
-    */
+    const codeList=['ko','zh-CN','en','ja','es','fr','it','ru','de','th','vi','id'];
+    const koreaList=['한국어','중국어','영어','일본어','스페인어','프랑스어','이탈리아어','러시아어','독일어','태국어','베트남어','인도네시아어'];
+    const englishList=['Korean','Chinese','English','Japanese','Spanish','French','Italian','Russian','German','Thai','Vietnamese','Indonesian'];
+    try{
+        for(const i in iconList) await sendMsg.react(iconList[i]);
+    } catch(e){ // emoji error
+        throw e;
+    }
+    const userFilter=(user)=>{
+        return user.author.id==msg.author.id;
+    };
+    const emojiFilter=(reaction)=>{
+        return iconList.includes(reaction.emoji.name)
+    };
+    let translatdLanguage=1; // 번역할 언어
+    let timeState=true;
+    sendMsg.awaitReactions(emojiFilter,{MAX: 1,time: 60000,errors: [`time`]}).then(collected=>{
+        const reaction=collected.first();
+        for(const i in iconList){
+            if(reaction.emoji.name==iconList[i]){
+                console.log(i);
+                translatdLanguage=i;
+                break;
+            }
+        }
+        sendMsg.reactions.removeAll().catch(e=>{throw e;});
+    }).catch(()=>{ // time out
+        // sendMsg.reactions.removeAll().catch(e=>{throw e;});
+        embed.fields=[];
+        if(msg.content==="파파고"){
+            embed.setTitle(`시간초과!`);
+        } else if(msg.content==="papago"){
+            embed.setTitle(`Time Over!`);
+        }
+        sendMsg.edit(embed);
+        timeState=false;
+    });
+    if(timeState==false) return;
+    // Translate Message Update
+    embed.fields=[];
+    if(msg.content==="파파고"){
+        await embed.setTitle(`번역할 내용을 입력해주세요.`);
+    } else if(msg.content==="papago"){
+        await embed.setTitle(`Please enter what you want to translate.`);
+    }
+    await sendMsg.edit(embed);
+    // TextAwait
+    let receiveText;
+    await msg.channel.awaitMessages(userFilter,{max: 1,time: 60000,errors: [`time`]}).then(collected=>{
+        receiveText=collected.first().content;
+    }).catch(()=>{ // time out
+        embed.fields=[];
+        if(msg.content==="파파고"){
+            embed.setTitle(`시간초과!`);
+        } else if(msg.content==="papago"){
+            embed.setTitle(`Time Over!`);
+        }
+        sendMsg.edit(embed);
+        timeState=false;
+    });
+    if(timeState==false) return;
+    // Translate using naver api
+    const LanguageCode=await papago.DetectLanguage(receiveText);
+    const TransText=await papago.Translation(LanguageCode.langCode,codeList[translatdLanguage],receiveText);
+    // Update Translate Message
+    let srcLanguage;
+    let tarLanguage;
+    for(const i in codeList){
+        if(TransText.message.result.srcLangType===codeList[i]) srcLanguage=i;
+        if(TransText.message.result.tarLangType===codeList[i]) tarLanguage=i;
+    }
+    embed.fields=[];
+    if(msg.content==="파파고"){
+        await embed.setTitle(`${koreaList[srcLanguage]} → ${koreaList[tarLanguage]}`)
+        .addField(`입력한 언어 : ${koreaList[srcLanguage]}`,receiveText)
+        .addField(`번역된 언어 : ${koreaList[tarLanguage]}`,TransText.message.result.translatedText);
+    } else if(msg.content==="papago"){
+        await embed.setTitle(`${englishList[srcLanguage]} → ${englishList[tarLanguage]}`)
+        .addField(`Input language : ${englishList[srcLanguage]}`,receiveText)
+        .addField(`translated language : ${englishList[tarLanguage]}`,TransText.message.result.translatedText);
+    }
+    await sendMsg.edit(embed);
 }
--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-
 const Discord=require('discord.js');
-const fs=require('fs');
-const colors=require('colors');
+require('dotenv').config();
 
 const {embed}=require('./utils/embed');
 const {ping}=require('./commands/ping');
 const {help}=require('./commands/help');
 const {translate}=require('./commands/translate');
 
-require('dotenv').config();
-
 if(process.env.discordToken){
     const discord=new Discord.Client();
     discord.on('ready',()=>{
-        console.log(colors.green(`Login in as ${discord.user.tag}`));
+        console.log(require('colors').green(`Login in as ${discord.user.tag}`));
     });
     discord.on('message',async msg=>{
         try{
@@ -21,10 +17,10 @@ if(process.env.discordToken){
             if(msg.content.includes('파파고')||msg.content.includes('papago')){
                 help(msg,embed(msg,Discord));
                 ping(msg,embed(msg,Discord),discord);
-                translate(msg,embed(msg,Discord),discord);
+                translate(msg,embed(msg,Discord));
             }
-        }catch(e){
-            console.warn(e);
+        } catch(e){
+            throw e;
         }
     });
     discord.login(process.env.discordToken);
