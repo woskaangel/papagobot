994e82fa28786d7ca2044cc0af571d59b7431c7f
woskaangel <52520428+woskaangel@users.noreply.github.com>
2020-10-06 11:08:49 +0900

    Update Activity
    *개의 서버에서 사용 중 추가

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ if(process.env.discordToken){
     const discord=new Discord.Client();
     discord.on('ready',()=>{
         console.log(require('colors').green(`Login in as ${discord.user.tag}`));
+        discord.user.setActivity(`${discord.guilds.cache.size}개의 서버에서 번역 중`);
     });
     discord.on('message',async msg=>{
         try{
